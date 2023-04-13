fcb29ba9ebadd88ac0a781d38b69c641adb55d73
Linorman <3033797357@qq.com>
2023-04-13 09:56:22 +0800

    Initial commit

---
+++ src/main/java/com/softwarelearn/lite_mall_backend/LiteMallBackendApplication.java
@@ -0,0 +1,13 @@
+package com.softwarelearn.lite_mall_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteMallBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteMallBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softwarelearn/lite_mall_backend/LiteMallBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarelearn.lite_mall_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteMallBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
