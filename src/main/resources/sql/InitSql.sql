8931a92867af3c1aebb85029408667bac79dadc9
Linorman <3033797357@qq.com>
2023-04-20 16:49:09 +0800

    修改sql文件

--- src/main/resources/sql/InitSql.sql
+++ src/main/resources/sql/InitSql.sql
@@ -2,24 +2,27 @@ create database user_info;
 create database goods_info;
 create database visit_order_info;
 use user_info;
-
-CREATE TABLE IF NOT EXISTS `user_info` (
- `id` INTEGER NOT NULL,
- `username` VARCHAR(255) NOT NULL,
- `password` VARCHAR(255) NOT NULL,
- `image_url` VARCHAR(255) NOT NULL,
- `phone_number` VARCHAR(255) NOT NULL,
- `account` INTEGER NOT NULL,
- `is_logined` INTEGER NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+create table user_info
+(
+    id           int auto_increment
+        primary key,
+    username     varchar(255) not null,
+    password     varchar(255) not null,
+    image_url    varchar(255) null,
+    phone_number varchar(255) not null,
+    account      int          not null,
+    is_logined   int          not null
+);
 
 use goods_info;
-CREATE TABLE IF NOT EXISTS `goods_info`(
-                                          `id` INTEGER NOT NULL,
-                                          `goods_name` VARCHAR(255) NOT NULL,
-                                          `goods_price` DOUBLE PRECISION NOT NULL,
-                                          `message` VARCHAR(255) NOT NULL,
-                                          PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+create table goods_info
+(
+    id          int auto_increment
+        primary key,
+    goods_name  varchar(255) not null,
+    goods_price double       not null,
+    message     varchar(255) not null
+);
+
+
 
