5ba3ee61fd4110e02269865c69d88196feeda3d8
Linorman <3033797357@qq.com>
2023-05-25 15:03:06 +0800

    更新sql

--- src/main/resources/sql/InitSql.sql
+++ src/main/resources/sql/InitSql.sql
@@ -1,6 +1,11 @@
+drop database if exists user_info;
+drop database if exists goods_info;
+drop database if exists visit_order_info;
+drop database if exists online_user_info;
 create database user_info;
 create database goods_info;
 create database visit_order_info;
+create database online_user_info;
 use user_info;
 create table user_info
 (
@@ -21,8 +26,10 @@ create table goods_info
         primary key,
     goods_name  varchar(255) not null,
     goods_price double       not null,
-    message     varchar(255) not null
+    message     varchar(255) not null,
+    file_path  varchar(1023) not null
 );
+
 
 use visit_order_info;
 create table visit_num_info
@@ -37,6 +44,14 @@ create table order_num_info
     id        int auto_increment
         primary key,
     order_num int default 0 not null
+);
+
+use online_user_info;
+create table online_user_num_info
+(
+    id           int auto_increment
+        primary key,
+    online_user_num int default 0 not null
 );
 
 
